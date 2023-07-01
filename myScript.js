f8c32e2d43dde3b4ece676cb10f63613b61f187c
FrootNoot <136702180+FrootNoot@users.noreply.github.com>
2023-07-01 13:21:57 +1200

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,21 +1,72 @@
-const elementobserver = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        if (entry.isIntersecting) {
-            const classList = entry.target.classList;
-            if (classList.contains('hidden')) {
-                classList.add('show');
-            } else if (classList.contains('heading-underline')) {
-                classList.add('heading-show');
-            }else if (classList.contains('fade-in-animate')) {
-                classList.add('show-fade');
-            }
-            // Add more conditions for different classes and animation classes as needed
-        } 
-    });
+/* Code for the navbar, it removes the class when a link is clicked */
+function closeNavbar() {
+  var navCheckbox = document.getElementById("nav");
+  navCheckbox.checked = false;
+}
+
+var links = document.querySelectorAll(".nav-wrapper ul li a");
+links.forEach(function(link) {
+  link.addEventListener("click", closeNavbar);
 });
 
-const elements = document.querySelectorAll('.hidden, .heading-underline, .fade-in-animate');
+
+
+const elementobserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+          const classList = entry.target.classList;
+          if (classList.contains('hidden-slide')) {
+              classList.add('show');
+          } else if (classList.contains('heading-underline')) {
+              classList.add('heading-show');
+          }else if (classList.contains('fade-in-animate')) {
+              classList.add('show-fade');
+          }
+          // Add more conditions for different classes and animation classes as needed
+      } 
+  });
+});
+
+const elements = document.querySelectorAll('.hidden-slide, .heading-underline, .fade-in-animate');
 elements.forEach((el) => elementobserver.observe(el));
+
+
+function scrollToElement(elementId) {
+  var element = document.getElementById(elementId);
+  var elementRect = element.getBoundingClientRect();
+  var absoluteElementTop = elementRect.top + window.scrollY;
+  var middle = absoluteElementTop - (window.innerHeight / 8);
+  
+  window.scrollTo({ top: middle, behavior: "smooth" });
+}
+
+document.querySelectorAll("[data-scroll-to]").forEach(function(button) {
+  button.addEventListener("click", function() {
+    var elementId = button.getAttribute("data-scroll-to");
+    scrollToElement(elementId);
+  });
+});
+
+
+
+function scrollDownHandler() {
+  let arrow = document.getElementById("arrow");
+  arrow.classList.add("fade-out");
+  arrow.classList.remove("fade-in");
+}
+
+var lastScrollPosition = 0;
+
+window.addEventListener("scroll", function() {
+  var currentScrollPosition = window.scrollY || document.documentElement.scrollTop;
+
+  if (currentScrollPosition > lastScrollPosition) {
+    scrollDownHandler();
+  }
+
+  lastScrollPosition = currentScrollPosition;
+});
+
 
 const contactForm = document.querySelector("#contact-form");
 const submitBtn = document.querySelector(".submit-btn");
@@ -49,48 +100,3 @@ contactForm.addEventListener("submit", e=>{
     });
 
 });
-
-const navbar = document.querySelector('.navbar')
-
-navbar.querySelector('.toggle').addEventListener('click',()=>{
-	
-	navbar.classList.toggle('collapsed')
-	
-})
-
-
-function scrollToElement(elementId) {
-    var element = document.getElementById(elementId);
-    var elementRect = element.getBoundingClientRect();
-    var absoluteElementTop = elementRect.top + window.scrollY;
-    var middle = absoluteElementTop - (window.innerHeight / 8);
-    
-    window.scrollTo({ top: middle, behavior: "smooth" });
-  }
-  
-  document.querySelectorAll("[data-scroll-to]").forEach(function(button) {
-    button.addEventListener("click", function() {
-      var elementId = button.getAttribute("data-scroll-to");
-      scrollToElement(elementId);
-    });
-  });
-
-
-
-function scrollDownHandler() {
-    let arrow = document.getElementById("arrow");
-    arrow.classList.add("fade-out");
-    arrow.classList.remove("fade-in");
-  }
-  
-  var lastScrollPosition = 0;
-  
-  window.addEventListener("scroll", function() {
-    var currentScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-  
-    if (currentScrollPosition > lastScrollPosition) {
-      scrollDownHandler();
-    }
-  
-    lastScrollPosition = currentScrollPosition;
-  });
